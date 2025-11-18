4feff92117b634a7581f3a570476fa50ee6c8774
Brandon Velandia <brandondavid.velandia@saluta.app>
2025-11-18 17:51:37 -0500

    Arreglar API y workflow para Azure

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,6 +1,5 @@
 module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    body: { message: "API funcionando correctamente en Azure 🚀" }
-  };
+    context.res = {
+        body: "Hola desde Azure Functions!"
+    };
 };
